3b19d0a95382603da6b3f61bf2d15fe7a2d62bbb
Povilas Galcius <povilas.galcius@devbridge.com>
2018-01-27 01:36:21 +0200

    Added praeivis health and reaction to semke

--- Assets/Scripts/Praeivis.cs
+++ Assets/Scripts/Praeivis.cs
@@ -1,101 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Pathfinding;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Seeker))]
 public class Praeivis : MonoBehaviour
 {
-    public Transform target;
-    public float updateRate = 2f;
+    public double Health = 2;
 
-    public Seeker seeker;
-
-    public Rigidbody2D rb;
-
-    public Path path;
-
-    public float speed;
-    public ForceMode2D fMode;
-
-    public bool pathIsEnded = false;
-
-    public float nextWaypointDistance = 3;
-
-    private int currentWaypoint = 0;
-
-    // Use this for initialization
-    void Start ()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        seeker = GetComponent<Seeker>();
-        rb = GetComponent<Rigidbody2D>();
-
-        if (target == null)
+        if (other.gameObject.tag == "Projectile")
         {
-            return;
-        }
-
-        // Start new path
-        seeker.StartPath(transform.position, target.position, OnPathComplete);
-
-        StartCoroutine(UpdatePath());
-    }
-
-    private IEnumerator UpdatePath()
-    {
-        if (target != null)
-        {
-            seeker.StartPath(transform.position, target.position, OnPathComplete);
-
-            yield return new WaitForSeconds( 1f/updateRate);
-            StartCoroutine(UpdatePath());
-        }
-    }
-
-    private void OnPathComplete(Path p)
-    {
-        if (!p.error)
-        {
-            path = p;
-            currentWaypoint = 0;
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (target == null || path == null)
-        {
-            Debug.Log("No target or path.");
-            return;
-        }
-
-        if (currentWaypoint >= path.vectorPath.Count)
-        {
-            if (pathIsEnded)
+            Health -= other.gameObject.GetComponent<Skill>().Damage;
+            if (Health <= 0)
             {
-                return;
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+                //GetComponent<Collider2D>().enabled = false;
+                //Speed = 0;
+                //anim.SetBool("Dies", true);
+                //GetComponent<Rigidbody2D>().gravityScale = 0.5f;
+                //GetComponent<MexicanMoving>().enabled = false;
             }
-
-            Debug.Log("End of path reached.");
-            pathIsEnded = true;
-            return;
         }
 
-        pathIsEnded = false;
-
-        Vector3 dir = path.vectorPath[currentWaypoint] - transform.position;
-        dir *= speed *Time.fixedDeltaTime;
-
-        rb.velocity = dir;
-        //rb.AddForce(dir, fMode);
-
-        var dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
-        if (dist < nextWaypointDistance)
-        {
-            currentWaypoint++;
-            return;
-        }
     }
 }
-;
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+
+    public double Damage = 1;
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left * 3;
+    }
+}
