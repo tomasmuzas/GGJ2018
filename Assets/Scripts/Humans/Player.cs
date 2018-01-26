6fa2fe2e9e7acf61a49e60883ef6696e5896d0b4
tomasmuzas <m_tomas@inbox.lt>
2018-01-27 01:30:15 +0200

    Add shooting mechanics

--- Assets/Scripts/Humans/Player.cs
+++ Assets/Scripts/Humans/Player.cs
@@ -1,36 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public enum Direction
 {
     Left, Right
 }
 
+public enum ShootingType
+{
+    Skill, PowerUp
+}
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour, IMovable, IHuman {
     
     private Direction direction;
-    public PowerUp PowerUp { get; private set; }
-    public Skill Skill { get; set; }
+    public GameObject PowerUp;
+    public GameObject SkillPrefab;
+
 
     [SerializeField]
     public int HP{ get; private set; }
     public float Speed { get; set; } 
 
     private new Rigidbody2D rigidbody;
-    
-    public void ApplyPowerUp(PowerUp newPowerUp)
-    {
-        PowerUp = newPowerUp;
-    }
-
-    public void ChooseSkill(Skill newSkill)
-    {
-        Skill = newSkill;
-    }
 
     public void Shoot()
     {
-        IDamageDealer damageDealer = PowerUp;
+        if(SkillPrefab != null)
+        {
+            Instantiate(SkillPrefab, transform.position, transform.rotation);
+            SpriteRenderer projectileSprite = SkillPrefab.GetComponent<SpriteRenderer>();
+            Rigidbody2D projectileRigidBody = SkillPrefab.GetComponent<Rigidbody2D>();
+            if (projectileSprite != null && projectileRigidBody != null)
+            {
+                Skill skillScript = SkillPrefab.GetComponent<Skill>();
+                projectileSprite.sprite = skillScript.ShootingSprite;
+                skillScript.Move();
+            }
+
+        }
     }
 
     private void Start()
@@ -43,6 +52,15 @@ public class Player : MonoBehaviour, IMovable, IHuman {
     private void Update()
     {
         Move();
+        CheckForShooting();
+    }
+
+    private void CheckForShooting()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
     }
 
     public void Move()
@@ -53,3 +71,4 @@ public class Player : MonoBehaviour, IMovable, IHuman {
         rigidbody.velocity = new Vector2(horizontalSpeed * Speed, verticalSpeed * Speed);
     }
 }
+
--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "Skill", menuName = "Skills/Skill")]
-public class Skill : ScriptableObject, IDamageDealer
+public class Skill : MonoBehaviour, IDamageDealer, IMovable
 {
     public Sprite UISprite;
     public Sprite ShootingSprite;
@@ -9,7 +9,17 @@ public class Skill : ScriptableObject, IDamageDealer
     private double _damage;
     public double Damage { get { return _damage; } private set { _damage = value; }}
     [SerializeField]
-    private double _speed;
-    public double Speed { get { return _speed; } private set { _speed = value; } }
+    private float _speed;
+    public float Speed { get { return _speed; } private set { _speed = value; } }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, 0);
+    }
 }
 
