67fb899041712497ef2019cbb101a4f73a38e2b6
Povilas Galcius <povilas.galcius@devbridge.com>
2018-01-27 07:28:42 +0200

    Babushka attack

--- Assets/Scripts/Humans/GrandmaVision.cs
+++ Assets/Scripts/Humans/GrandmaVision.cs
@@ -2,7 +2,7 @@
 
 public class GrandmaVision : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
