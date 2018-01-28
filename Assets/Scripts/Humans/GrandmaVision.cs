116e802a4252a9f0373ba7a935c828af9f9d91c0
Povilas Galcius <povilas.galcius@devbridge.com>
2018-01-28 12:24:41 +0200

    Sounds and skill

--- Assets/Scripts/Humans/GrandmaVision.cs
+++ Assets/Scripts/Humans/GrandmaVision.cs
@@ -1,14 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class GrandmaVision : MonoBehaviour {
+public class GrandmaVision : MonoBehaviour
+{
+    private bool activated = true;
+    public float activeTime = 2f;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && activated)
         {
             Player player = collision.gameObject.GetComponent<Player>();
             player.DecreaseHP();
+            PlayTransformationSound();
+            activated = false;
+            StartCoroutine(ActivateAgain());
         }
     }
 
+    void PlayTransformationSound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    public IEnumerator ActivateAgain()
+    {
+        yield return new WaitForSeconds(activeTime);
+        activated = true;
+    }
 }
